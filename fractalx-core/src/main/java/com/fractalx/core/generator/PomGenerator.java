72bd360244f3d5b69adca09d435edd3d7fcf2869
Dinuvi Asithma <127608354+IT22356086@users.noreply.github.com>
2026-01-05 12:00:05 +0530

    Request logging with trace correlation

--- fractalx-core/src/main/java/com/fractalx/core/generator/PomGenerator.java
+++ fractalx-core/src/main/java/com/fractalx/core/generator/PomGenerator.java
@@ -4,7 +4,6 @@ import com.fractalx.core.FractalModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.fractalx.core.observability.ObservabilityInjector;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
